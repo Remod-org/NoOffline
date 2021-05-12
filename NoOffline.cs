19cfc7b7dee68d168c782c24e353bc2656857b9b
Remod <desolationoutpostpve@gmail.com>
2021-05-12 06:10:54 -0500

    Initial import

---
+++ NoOffline.cs
@@ -0,0 +1,145 @@
+﻿using Oxide.Core;
+using Oxide.Core.Plugins;
+using System.Linq;
+
+namespace Oxide.Plugins
+{
+    [Info("NoOffline", "RFC1920", "1.0.2")]
+    [Description("Disables offline raid based on building privilege members, friends, clans, or teams")]
+    class NoOffline : RustPlugin
+    {
+        [PluginReference]
+        private readonly Plugin Friends, Clans, RustIO;
+        private ConfigData configData;
+
+        private object OnEntityTakeDamage(BaseCombatEntity entity, HitInfo hitinfo)
+        {
+            if (entity == null) return null;
+            if (hitinfo.damageTypes.GetMajorityDamageType().ToString() == "Decay") return null;
+
+            bool foundOnline = false;
+            BuildingPrivlidge tc = entity.GetBuildingPrivilege();
+
+            if (tc != null)
+            {
+                DoLog("Checking authorized players");
+                foreach (ulong playerid in tc.authorizedPlayers.Select(x => x.userid).ToArray())
+                {
+                    DoLog($"Checking player {playerid.ToString()}");
+                    BasePlayer player = BasePlayer.Find(playerid.ToString()) ?? null;
+                    if (player != null)
+                    {
+                        if (BasePlayer.activePlayerList.Contains(player))
+                        {
+                            DoLog($"Damage allowed because an authorized player, {player.displayName}, is online.");
+                            foundOnline = true;
+                        }
+                    }
+                    if (configData.Options.HonorRelationships)
+                    {
+                        foreach (BasePlayer peep in BasePlayer.activePlayerList)
+                        {
+                            if (IsFriend(peep.userID, playerid))
+                            {
+                                DoLog($"Damage allowed because a friend of an authorized player, {peep.displayName}, is online.");
+                                foundOnline = true;
+                            }
+                        }
+                    }
+                }
+                if (foundOnline) return null; // Normal behavior
+                else return true;
+            }
+
+            return null;
+        }
+
+        private void DoLog(string message)
+        {
+            if (configData.Options.debug) Interface.Oxide.LogInfo(message);
+        }
+
+        private bool IsFriend(ulong playerid, ulong ownerid)
+        {
+            if (configData.Options.useFriends && Friends != null)
+            {
+                object fr = Friends?.CallHook("AreFriends", playerid, ownerid);
+                if (fr != null && (bool)fr)
+                {
+                    return true;
+                }
+            }
+            if (configData.Options.useClans && Clans != null)
+            {
+                string playerclan = (string)Clans?.CallHook("GetClanOf", playerid);
+                string ownerclan = (string)Clans?.CallHook("GetClanOf", ownerid);
+                if (playerclan != null && ownerclan != null)
+                {
+                    if (playerclan == ownerclan)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (configData.Options.useTeams)
+            {
+                BasePlayer player = BasePlayer.FindByID(playerid);
+                if (player != null)
+                {
+                    if (player.currentTeam != 0)
+                    {
+                        RelationshipManager.PlayerTeam playerTeam = RelationshipManager.Instance.FindTeam(player.currentTeam);
+                        if (playerTeam != null)
+                        {
+                            if (playerTeam.members.Contains(ownerid))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        #region config
+        private void LoadConfigVariables()
+        {
+            configData = Config.ReadObject<ConfigData>();
+
+            configData.Version = Version;
+            SaveConfig(configData);
+        }
+
+        protected override void LoadDefaultConfig()
+        {
+            Puts("Creating new config file.");
+            ConfigData config = new ConfigData
+            {
+                Version = Version,
+            };
+
+            SaveConfig(config);
+        }
+        private void SaveConfig(ConfigData config)
+        {
+            Config.WriteObject(config, true);
+        }
+
+        public class ConfigData
+        {
+            public Options Options = new Options();
+            public VersionNumber Version;
+        }
+
+        public class Options
+        {
+            public bool debug = false;
+            public bool useFriends = false;
+            public bool useClans = false;
+            public bool useTeams = false;
+            public bool HonorRelationships = false;
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # NoOffline
-Experimental plugin to prevent offline raiding based on privilege owners, members, and friends of those players
+EXPERIMENTAL plugin to prevent offline raiding based on privilege owners, members, and friends of those players
+
+This is not for use on PVE servers and/or servers using a PVE plugin.
+
+On every attempt to damage an entity within building privilege, we check for the following:
+
+  1. Any authorized user is currently online
+  2. Any friend, clan member, or teammate of an authorized player is online (based on useClans/useFriends/useTeams).
+
+If either of the above are true, damage is allowed.
+If all players, members, etc. are offline, damage should be prevented.
+
+## Configuration
+```json
+{
+  "Options": {
+    "debug": false,
+    "HonorRelationships": false,
+    "useClans": false,
+    "useFriends": false,
+    "useTeams": false
+  },
+  "Version": {
+    "Major": 1,
+    "Minor": 0,
+    "Patch": 2
+  }
+}
+```
+
+  - `HonorRelationships` -- If set, honor any of the useXXX features to determine online status
+  - `useClans` -- Use various Clans plugins for determining relationships.
+  - `useFriends` -- Use various Friends plugins for determining relationships.
+  - `useTeams` -- Use Rust native teams for determining relationships.
